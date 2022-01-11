8958477c7b590d972876eefa8dc38f8995d9a11a
Yash Tembhare <97080152+YashTembhare@users.noreply.github.com>
2022-01-11 23:42:11 +0530

    Update AddressBook.java

--- Addressbook/src/Addressbook/AddressBook.java
+++ Addressbook/src/Addressbook/AddressBook.java
@@ -1,6 +1,7 @@
 package Addressbook;
 
 import java.util.*;
+import java.io.*;
 
 public class AddressBook {
 
@@ -12,25 +13,28 @@ public AddressBook() {
     this.listOfEntries = new ArrayList<>();
 }
 
-public AddressBookData[] viewAll() {
+public AddressBookData[] viewAll() throws FileNotFoundException, IOException {
 	//create a new array with the size of our list
     AddressBookData[] result = new AddressBookData[this.listOfEntries.size()];
-    
     this.listOfEntries.toArray(result);
-
     //return the filled array
     return result;
 }
 
 
-public void add(AddressBookData AddressBookData) {
+public void add(AddressBookData AddressBookData) throws FileNotFoundException, IOException {
     //Add new entry
     this.listOfEntries.add(AddressBookData);
+    ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("File.txt"));
+    out.writeObject(AddressBookData);
 }
 
-public void delete(int index) {
+public void delete(int index) throws FileNotFoundException, IOException {
     //Delete new entry
     this.listOfEntries.remove(index);
+    ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("File.txt"));
+    out.writeObject(index);
+    
 }
 
 public AddressBookData get(int index) {
