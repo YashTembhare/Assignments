6087f12eaf33e2b1a090870508b78c01780d5527
Yash Tembhare <97080152+YashTembhare@users.noreply.github.com>
2022-01-19 20:47:46 +0530

    Update AddressBook.java

--- Addressbook/src/Addressbook/AddressBook.java
+++ Addressbook/src/Addressbook/AddressBook.java
@@ -1,4 +1,4 @@
-package Addressbook;
+package AddressBook;
 
 import java.util.*;
 import java.io.*;
@@ -6,41 +6,45 @@ import java.lang.reflect.Array;
 
 public class AddressBook {
 
-//AddressBookData Objects stored in list
-private List<AddressBookData> listOfEntries;
+    //AddressBookData Objects stored in list
+    private List<AddressBookData> listOfEntries;
 
-public AddressBook() {
-	//initialize list of entries
-    this.listOfEntries = new ArrayList<>();
-}
+    public AddressBook() {
+        //initialize list of entries
+        this.listOfEntries = new ArrayList<>();
+    }
 
-public AddressBookData[] viewAll() throws FileNotFoundException, IOException {
-	//create a new array with the size of our list
-    AddressBookData[] result = new AddressBookData[this.listOfEntries.size()];
-    this.listOfEntries.toArray(result);
-    //return the filled array
-    return result;
-}
+    public AddressBookData[] viewAll() throws FileNotFoundException, IOException {
+        //create a new array with the size of our list
+        AddressBookData[] result = new AddressBookData[this.listOfEntries.size()];
+        HashSet<AddressBookData> hashSet = new HashSet<>(listOfEntries);
+        hashSet.toArray(result);
+        //return the filled array
+        return result;
+    }
 
 
-public void add(AddressBookData AddressBookData) throws FileNotFoundException, IOException {
-    //Add new entry
-    this.listOfEntries.add(AddressBookData);
-    FileWriter fileWriter = new FileWriter("File.txt", true);
-    fileWriter.write("\n" + AddressBookData.toString());
-    fileWriter.close();
-    System.out.println("\nSuccess ...!!");
+    public void add(AddressBookData AddressBookData) throws FileNotFoundException, IOException {
+        //Add new entry
+        this.listOfEntries.add(AddressBookData);
+        FileWriter fileWriter = new FileWriter("File.txt", true);
+        fileWriter.write("\n" + AddressBookData.toString());
+        fileWriter.close();
+
+    }
+
     
-}
+    public void delete(int index) throws FileNotFoundException, IOException {
+        //Delete new entry
+        this.listOfEntries.remove(index);
+    }
 
-public void delete(int index) throws FileNotFoundException, IOException {
-    //Delete new entry
-    this.listOfEntries.remove(index);
-}
+    
+    public AddressBookData get(int index) {
+        //give entry of inputed index
+        return this.listOfEntries.get(index);
+    }
+    
 
-public AddressBookData get(int index) {
-    //give entry of inputed index
-    return this.listOfEntries.get(index);
-}
 
 }
